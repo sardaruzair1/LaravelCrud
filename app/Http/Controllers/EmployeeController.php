1263fec7d6c024161b8033c12e146ffd9afe20bd
sardaruzair1 <sardarsafeer012012@gmail.com>
2023-02-17 00:26:17 +0500

    model and controller and also hello world testing

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EmployeeController extends Controller
+{
+    //
+    public function index(){
+        echo "This is just for testing";
+    }
+    
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\EmployeeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/employees',[EmployeeController::class,'index'])->name('employees.index');
